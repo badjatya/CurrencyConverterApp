7ba92bb973f19e8fcc94a2b51464146b79aa1f58
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-14 23:07:34 +0530

    RowItem component created

---
+++ App/components/RowItem.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+
+import Colors from "../constants/Colors";
+
+const RowItem = ({ rightIcon, text }) => {
+  return (
+    <TouchableOpacity style={styles.row}>
+      <Text style={styles.text}>{text}</Text>
+      {rightIcon}
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  row: {
+    marginHorizontal: 20,
+    marginVertical: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 16,
+    color: Colors.text,
+  },
+});
--- App/screens/Options.js
+++ App/screens/Options.js
@@ -40,17 +40,7 @@ const styles = StyleSheet.create({
   container: {
     marginTop: StatusBar.currentHeight,
   },
-  row: {
-    marginHorizontal: 20,
-    marginVertical: 16,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  text: {
-    fontSize: 16,
-    color: Colors.text,
-  },
+
   separator: {
     backgroundColor: Colors.border,
     height: StyleSheet.hairlineWidth,
