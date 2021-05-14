ab32417290f7c28afe6cd0a643c28ffdafb2490a
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-14 22:24:22 +0530

    Options screen

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -4,13 +4,24 @@ import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 const Options = () => {
   return (
     <View>
-      <TouchableOpacity>
-        <Text></Text>
+      <TouchableOpacity style={styles.row}>
+        <Text style={styles.text}>React Native</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.row}>
+        <Text style={styles.text}>React Native Basics</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.row}>
+        <Text style={styles.text}>React Native by Examples</Text>
       </TouchableOpacity>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {},
+  text: {},
+});
 
 export default Options;
