5749b26e6618ee7c7ae7e44909e8141989c48d74
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-14 23:51:02 +0530

    ScrollView Added

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, StatusBar, Alert } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  Alert,
+  ScrollView,
+} from "react-native";
 import { Entypo } from "@expo/vector-icons";
 
 import Colors from "../constants/Colors";
@@ -8,25 +14,27 @@ import { RowItem, RowSeparator } from "../components/RowItem";
 const Options = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <RowItem
-        text="React Native"
-        rightIcon={
-          <Entypo name="chevron-right" size={20} color={Colors.blue} />
-        }
-        onPress={() => alert("TODO!!")}
-      />
-      <RowSeparator />
-      <RowItem
-        text="React Native Basics"
-        rightIcon={<Entypo name="export" size={20} color={Colors.blue} />}
-        onPress={() => alert("!!")}
-      />
-      <RowSeparator />
-      <RowItem
-        text="React Native by Examples"
-        rightIcon={<Entypo name="export" size={20} color={Colors.blue} />}
-        onPress={() => alert("!!")}
-      />
+      <ScrollView>
+        <RowItem
+          text="React Native"
+          rightIcon={
+            <Entypo name="chevron-right" size={20} color={Colors.blue} />
+          }
+          onPress={() => alert("TODO!!")}
+        />
+        <RowSeparator />
+        <RowItem
+          text="React Native Basics"
+          rightIcon={<Entypo name="export" size={20} color={Colors.blue} />}
+          onPress={() => alert("!!")}
+        />
+        <RowSeparator />
+        <RowItem
+          text="React Native by Examples"
+          rightIcon={<Entypo name="export" size={20} color={Colors.blue} />}
+          onPress={() => alert("!!")}
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 };
