e0328f541fb22a7717c04ba255b4de02019c82a2
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-14 22:19:43 +0530

    Added screen Options

---
+++ App/screens/Options.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+const Options = () => {
+  return (
+    <View>
+      <TouchableOpacity>
+        <Text></Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Options;
