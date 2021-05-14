6fa97ec2e41f83d94afc93643e8eca35b1fd60c1
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-14 22:52:55 +0530

    Imported Entypo icons

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -7,6 +7,7 @@ import {
   StyleSheet,
   StatusBar,
 } from "react-native";
+import { Entypo } from "@expo/vector-icons";
 
 import Colors from "../constants/Colors";
 
@@ -15,18 +16,21 @@ const Options = () => {
     <SafeAreaView style={styles.container}>
       <TouchableOpacity style={styles.row}>
         <Text style={styles.text}>React Native</Text>
+        <Entypo name="chevron-right" size={20} color={Colors.blue} />
       </TouchableOpacity>
 
       <View style={styles.separator} />
 
       <TouchableOpacity style={styles.row}>
         <Text style={styles.text}>React Native Basics</Text>
+        <Entypo name="export" size={20} color={Colors.blue} />
       </TouchableOpacity>
 
       <View style={styles.separator} />
 
       <TouchableOpacity style={styles.row}>
         <Text style={styles.text}>React Native by Examples</Text>
+        <Entypo name="export" size={20} color={Colors.blue} />
       </TouchableOpacity>
     </SafeAreaView>
   );
@@ -39,6 +43,9 @@ const styles = StyleSheet.create({
   row: {
     marginHorizontal: 20,
     marginVertical: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   text: {
     fontSize: 16,
