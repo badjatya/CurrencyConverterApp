00d7216572fc4b60c18d2a05035b7339b44fa4a6
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-14 22:22:20 +0530

    Options imported in App

--- App/index.js
+++ App/index.js
@@ -1,21 +1,10 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import Options from "./screens/Options";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const App = () => {
+  return <Options />;
+};
+
+export default App;
