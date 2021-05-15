a21ab516fee4df67cc4cf5eaac1ec1fb3182579e
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-15 22:29:05 +0530

    ConversionInput component added

---
+++ App/components/ConversionInput.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+} from "react-native";
+
+const ConversionInput = () => {
+  return (
+    <View>
+      <Text> Hii</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default ConversionInput;
