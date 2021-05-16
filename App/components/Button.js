bd69b8ddd02caafc9ce1c0b0c7d92dcf04af9204
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-16 12:30:58 +0530

    Button Component for reversing currencies

---
+++ App/components/Button.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { TouchableOpacity, Text, Image, StyleSheet } from "react-native";
+
+import Colors from "../constants/Colors";
+
+const styles = StyleSheet.create({
+  button: {},
+  buttonIcon: {},
+  buttonText: {},
+});
+
+const Button = ({ text, onPress }) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Image
+        source={require("../assets/images/reverse.png")}
+        style={styles.buttonIcon}
+      />
+      <Text style={styles.buttonText}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
