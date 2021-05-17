f9b9f9405e2701710ade9590594f3809e97ce498
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-17 20:24:02 +0530

    Modal stack navigation for currency list screen

--- App/config/Navigation.js
+++ App/config/Navigation.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { TouchableOpacity } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Entypo } from "@expo/vector-icons";
 
 import Home from "../screens/Home";
 import Options from "../screens/Options";
 import CurrencyList from "../screens/CurrencyList";
+import Colors from "../constants/Colors";
 
 const MainStack = createStackNavigator();
 
@@ -20,21 +23,48 @@ const MainStackScreen = () => {
         options={{ headerShown: false }}
       />
       <MainStack.Screen name="Options" component={Options} />
-      <MainStack.Screen
+    </MainStack.Navigator>
+  );
+};
+
+const ModalStack = createStackNavigator();
+
+const ModalStackScreen = () => {
+  return (
+    <ModalStack.Navigator
+      mode="modal"
+      screenOptions={{ headerTitleAlign: "center" }}
+    >
+      <ModalStack.Screen
+        name="Main"
+        component={MainStackScreen}
+        options={{ headerShown: false }}
+      />
+
+      <ModalStack.Screen
         name="CurrencyList"
         component={CurrencyList}
-        options={({ route }) => ({
+        options={({ route, navigation }) => ({
           title: route.params.title,
+          headerLeft: null,
+          headerRight: () => (
+            <TouchableOpacity
+              style={{ padding: 20 }}
+              onPress={() => navigation.pop()}
+            >
+              <Entypo name="cross" size={30} color={Colors.blue} />
+            </TouchableOpacity>
+          ),
         })}
       />
-    </MainStack.Navigator>
+    </ModalStack.Navigator>
   );
 };
 
 export default () => {
   return (
     <NavigationContainer>
-      <MainStackScreen />
+      <ModalStackScreen />
     </NavigationContainer>
   );
 };
