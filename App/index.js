e2193a11c3ee168d5da8ca637b2aecf8a77f9844
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-18 15:40:50 +0530

    ConversionContextProvider is providing to navigation in Index js

--- App/index.js
+++ App/index.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import { ConversionContextProvider } from "./util/ConversionContext";
 import Navigation from "./config/Navigation";
 
 const App = () => {
-  return <Navigation />;
+  return (
+    <ConversionContextProvider>
+      <Navigation />
+    </ConversionContextProvider>
+  );
 };
 
 export default App;
