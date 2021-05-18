a71208b47b73fc67e115c53f60e1a00654fa3da9
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-18 21:48:05 +0530

    Using api in context API

--- App/util/ConversionContext.js
+++ App/util/ConversionContext.js
@@ -1,10 +1,27 @@
 import React, { createContext, useState } from "react";
 
+import { Alert } from "react-native";
+import { api } from "./api";
+
 export const ConversionContext = createContext();
 
 export const ConversionContextProvider = ({ children }) => {
-  const [baseCurrency, setBaseCurrency] = useState("USD");
+  const [baseCurrency, _setBaseCurrency] = useState("USD");
   const [quoteCurrency, setQuoteCurrency] = useState("GBP");
+  const [date, setDate] = useState();
+  const [rates, setRates] = useState({});
+
+  const setBaseCurrency = (currency) => {
+    return api(`/latest?base=${currency}`)
+      .then((res) => {
+        _setBaseCurrency(currency);
+        setDate(res.date);
+        setRates(res.rates);
+      })
+      .catch((error) => {
+        Alert.alert("Sorry, something went wrong.", error.message);
+      });
+  };
 
   const swapCurrency = () => {
     setBaseCurrency(quoteCurrency);
@@ -17,6 +34,8 @@ export const ConversionContextProvider = ({ children }) => {
     setBaseCurrency,
     setQuoteCurrency,
     swapCurrency,
+    date,
+    rates,
   };
 
   return (
