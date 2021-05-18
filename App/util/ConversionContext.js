fa1f1e137a7296c9a4e5d8b9e5bee932620dd888
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-18 15:37:47 +0530

    ConversionContext Provider and context created

--- App/util/ConversionContext.js
+++ App/util/ConversionContext.js
@@ -1,1 +1,27 @@
-import React, { useContext } from "react";
+import React, { createContext, useState } from "react";
+
+export const ConversionContext = createContext();
+
+export const ConversionContextProvider = ({ children }) => {
+  const [baseCurrency, setBaseCurrency] = useState("USD");
+  const [quoteCurrency, setQuoteCurrency] = useState("GBP");
+
+  const swapCurrency = () => {
+    setBaseCurrency(quoteCurrency);
+    setQuoteCurrency(baseCurrency);
+  };
+
+  const contextValue = {
+    baseCurrency,
+    quoteCurrency,
+    setBaseCurrency,
+    setQuoteCurrency,
+    swapCurrency,
+  };
+
+  return (
+    <ConversionContext.Provider value={contextValue}>
+      {children}
+    </ConversionContext.Provider>
+  );
+};
