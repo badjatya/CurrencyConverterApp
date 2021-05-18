4fdd9b4e30e7f4c2e469ffb2938728f226912ed9
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-18 21:43:13 +0530

    Remved api from index.js

--- App/index.js
+++ App/index.js
@@ -2,12 +2,6 @@ import React from "react";
 import { ConversionContextProvider } from "./util/ConversionContext";
 import Navigation from "./config/Navigation";
 
-import { api } from "./util/api";
-
-api("/latest?base=USD")
-  .then((res) => console.log(res))
-  .catch((err) => console.log("err", err));
-
 const App = () => {
   return (
     <ConversionContextProvider>
