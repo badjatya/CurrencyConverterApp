8c4e15c2ab61a3b02bba39267d52c90efe349213
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-18 21:33:52 +0530

    imported api in index file

--- App/index.js
+++ App/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import { ConversionContextProvider } from "./util/ConversionContext";
 import Navigation from "./config/Navigation";
 
+import { api } from "./util/api";
+
+api("/latest?base=USD")
+  .then((res) => console.log(res))
+  .catch((err) => console.log("err", err));
+
 const App = () => {
   return (
     <ConversionContextProvider>
